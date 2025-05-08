96b90ae47afae82ae0bb83054c3255adcd671a38
Peter Kerschbaumer <peter@kerschbaumer.se>
2025-05-08 12:55:25 +0200

    Added cards crud routes

--- backend/routers/card.go
+++ backend/routers/card.go
@@ -1,5 +1,14 @@
 package routers
 
-func RegisterCardRoutes() {
-	// http.HandleFunc("/cards/", handlers.CardRouter)
+import (
+	"net/http"
+
+	"github.com/Germanicus1/kanban-sim/handlers"
+)
+
+func InitCardRoutes() {
+	http.HandleFunc("/cards", handlers.CreateGame)        // POST /games
+	http.HandleFunc("/cards/get", handlers.GetGame)       // GET /games?id={id}
+	http.HandleFunc("/cards/update", handlers.UpdateGame) // PUT /games?id={id}
+	http.HandleFunc("/cards/delete", handlers.DeleteGame) // DELETE /games?id={id}
 }
